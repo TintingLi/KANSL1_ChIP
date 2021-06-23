c410434948c6eae8c36e7de6ac3b7b0a986ed5d1
TinTingLi <ting67@126.com>
2021-06-23 10:01:56 +0800

    Add files via upload

---
+++ plot.sh
@@ -0,0 +1,10 @@
+pyGenomeTracks --tracks stx17_tracks.ini --region chr9:102655000-102770000 -o image.png --dpi 300
+pyGenomeTracks --tracks stx17_tracks.ini --region chr9:102666920-102670314 -o image.png --dpi 300 -o zoomIn.png
+pyGenomeTracks --tracks stx17_tracks.bw.ini --region chr9:102655000-102770000 -o bw.png --dpi 300
+pyGenomeTracks --tracks pik3r4_tracks.bw.ini --region chr3:130350000-130500000 -o pik3r4.png --dpi 300
+pyGenomeTracks --tracks atg14_tracks.bw.ini --region chr14:55800000-55920000 -o atg14.png --dpi 300
+pyGenomeTracks --tracks atg14_tracks.bw.ini --region chr14:55800000-55920000 -o atg14.png --dpi 300
+pyGenomeTracks --tracks pik3r4_tracks.bw.ini --region chr3:130450000-130475000 -o pik3r4.zoom.png --dpi 300
+pyGenomeTracks --tracks atg14_tracks.bw.ini --region chr14:55860000-55890000 -o atg14.zoom.png --dpi 300
+pyGenomeTracks --tracks ykt6_tracks.bw.ini --region chr7:44235000-44260000 -o ykt6.png --dpi 300
+pyGenomeTracks --tracks vamp7_tracks.bw.ini --region chrX:155050000-155250000 -o VAMP7.png --dpi 300
